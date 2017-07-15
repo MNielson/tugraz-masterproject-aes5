04a95d4986d8295827223d6cb6730f62198eb994
Matthias Niel <matthias.niel@student.tugraz.at>
2017-07-15 15:25:53 +0200

    add helper functions. add pairs computation. add CLI. add performance timing.

--- aes5/Source/main.cpp
+++ aes5/Source/main.cpp
@@ -6,11 +6,18 @@
 
 #include "../Header/aesmain.h"
 #include <stdio.h>
+#include <iostream>
+#include <string>
 #include <wmmintrin.h>
 #include <thread>
 #include <mutex>
 #include <vector>
 #include <math.h>
+#include <iostream>
+#include <sstream>
+#include <cstdint>
+
+enum StepSizeSetting { FULL, PER_THREAD, TOTAL };
 
 #define KEYEXP(K, I) aes128_keyexpand(K, _mm_aeskeygenassist_si128(K, I))
 __m128i aes128_keyexpand(__m128i key, __m128i keygened)
@@ -34,13 +41,42 @@ void printM128i(__m128i* p)
 }
 
 
-void thrAes5(const uint32_t &threadNum, const uint32_t &numThreads, __m128i expKey[], uint8_t res[]);
+void thrAes5(const uint64_t startNum, const uint64_t stopNum, __m128i expKey[], uint8_t res[]);
+uint64_t workPerThread(const StepSizeSetting setting, const uint64_t numWork, const uint64_t numThreads);
+uint64_t totalWork(StepSizeSetting setting, uint64_t workload, uint64_t numThreads);
+
+void printUsage(std::string name);
+StepSizeSetting parseSetting(std::string setting);
 
 static std::mutex mtx;
 
-
-void main()
+void main(int argc, char* argv[])
 {
+	
+	StepSizeSetting stepSizeSetting = FULL;
+	uint64_t workload = 1;
+	uint64_t numThreads = 1;
+	if ( (argc == 1 || argc > 4) )
+	{
+		printUsage(argv[0]);
+		return;
+	}
+	else
+	{
+		stepSizeSetting = parseSetting(argv[1]);
+		if (argc >= 3)
+		{
+			std::istringstream workStr(argv[2]);
+			workStr >> workload;
+		}
+		if (argc == 4)
+		{
+			std::istringstream threadStr(argv[3]);
+			threadStr >> numThreads;
+		}
+	}
+
+	/*
 	AesMain aesMain;
 	uint8_t key[16] = { 0 };
 	uint8_t data[16] = { 0 };
@@ -54,7 +90,7 @@ void main()
 	aesMain.aesWithoutKeyExpansion(data, expandedKey, 10, false);
 	printf("\nData Out: \n");
 	aesMain.printBlock(data, 16);
-
+	*/
 
 	__m128i key128;
 	key128.m128i_u64[1] = 0x0000000000000000;
@@ -73,16 +109,30 @@ void main()
 	expKey[9]  = KEYEXP(expKey[8], 0x1B);
 	expKey[10] = KEYEXP(expKey[9], 0x36);
 
-	uint32_t numThreads =8;
 	std::vector<std::thread> threads;
 	uint64_t sz = (uint64_t)1 << 32;
 	uint8_t* res = new uint8_t[sz+1]();
 
+	uint64_t stepSize = workPerThread(stepSizeSetting, workload, numThreads);
+	uint64_t totalWorkNum = totalWork(stepSizeSetting, workload, numThreads);
+	
+	uint64_t startNum = 0;
+	uint64_t stopNum = stepSize - 1;
+	auto begin = std::chrono::high_resolution_clock::now();
 	//start worker threads
 	for (uint32_t threadNum = 0; threadNum < numThreads; ++threadNum)
 	{
-		threads.push_back( std::thread(thrAes5, std::ref(threadNum), std::ref(numThreads), std::ref(expKey), std::ref(res)) );
+		if (threadNum+1 == numThreads && stopNum < totalWorkNum - 1)
+		{
+			// make sure all remaining work is handed to last thread
+			// this can lead to last thread getting significantly more work than others (ex: wl 13, th 5)
+			stopNum = totalWorkNum - 1;
+		}
+		threads.push_back(std::thread(thrAes5, startNum, stopNum, std::ref(expKey), std::ref(res)));
+		startNum = stopNum + 1;
+		stopNum = startNum + stepSize - 1;		
 	}
+
 	// wait for worker threads to finish
 	for (auto &t : threads)
 	{
@@ -90,27 +140,106 @@ void main()
 	}
 
 	// do something with res
+	auto end = std::chrono::high_resolution_clock::now();
+	
+	std::cout << "Computed "<< totalWorkNum << " instances aes5 with " << numThreads << " threads in " << std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() << " seconds" << std::endl;
+
+	auto begin2 = std::chrono::high_resolution_clock::now();
+	uint64_t workDone = 0;
+	for (uint64_t i = 0; i <= sz; ++i)
+		workDone += res[i];
+	auto end2 = std::chrono::high_resolution_clock::now();
+	std::cout << "Detected " << workDone << " pieces of work done in " << std::chrono::duration_cast<std::chrono::milliseconds>(end2 - begin2).count() << " milliseconds" << std::endl;
+
+
+
+	auto begin3 = std::chrono::high_resolution_clock::now();
+	uint64_t pairs = 0;
+	uint64_t tmp = 0;
+	for (uint64_t i = 0; i <= sz; ++i)
+	{
+		tmp = 0;
+		if (res[i] > 1)
+			tmp = (res[i] * (res[i] - 1)) / 2;
+		pairs += tmp;
+	}
+		
+	uint64_t remainder = pairs % 8;
+	auto end3 = std::chrono::high_resolution_clock::now();
+	std::cout << "Detected " << pairs << " pairs in " << std::chrono::duration_cast<std::chrono::milliseconds>(end3 - begin3).count() << " milliseconds" << std::endl;
+
+	if (remainder == 0)
+		std::cout << pairs << " is a multiple of 8 :)" << std::endl;
+	else
+		std::cout << pairs << " is NOT a multiple of 8 :(" << std::endl;
+		
+
 
 	//cleanup
 	delete[] res;
+	
 
 
 }
+/*
+bool isUnevenWork(const uint32_t numWork, const uint32_t numThreads)
+{
+	if (numWork % numThreads == 0)
+		return false;
+	else
+		return true;
+}
+*/
 
+uint64_t workPerThread(const StepSizeSetting setting, const uint64_t numWork, const uint64_t numThreads)
+{
+	uint64_t stepSize = 0;
 
-void thrAes5(const uint32_t &threadNum, const uint32_t &numThreads, __m128i expKey[], uint8_t res[])
+	uint64_t maxWork = (uint64_t)1 << 32;
+
+	switch (setting)
+	{
+	case FULL:
+		stepSize = (uint64_t)floor(maxWork / numThreads);
+		break;
+
+	case PER_THREAD:
+		stepSize = numWork;
+		break;
+
+	case TOTAL:
+		stepSize = (uint64_t)floor(numWork / numThreads);
+		break;
+	}
+	return stepSize;
+}
+
+uint64_t totalWork(StepSizeSetting setting, uint64_t workload, uint64_t numThreads)
+{
+	uint64_t totalWork = 0;
+	switch (setting)
+	{
+	case FULL:
+		totalWork = (uint64_t)1 << 32;
+		break;
+	case TOTAL:
+		totalWork = workload;
+		break;
+	case PER_THREAD:
+		totalWork = workload * numThreads;
+		break;
+	}
+	return totalWork;
+}
+
+/*
+void getThreadWorkNums(const uint32_t threadNum, const uint32_t numThreads, const uint32_t stepSize)
 {
 	uint32_t startNum = 0;
-	uint32_t stopNum  = 0;
-	uint32_t stepSize = 0;
+	uint32_t stopNum = 0;
 	uint64_t sz = (uint64_t)1 << 32;
 
-#ifdef _DEBUG
-	stepSize = 100;
-#else
-	stepSize = (uint32_t)floor(sz / (uint64_t)numThreads);
-#endif
-	
+
 	if (threadNum == 0)
 	{
 		startNum = 0;
@@ -126,12 +255,15 @@ void thrAes5(const uint32_t &threadNum, const uint32_t &numThreads, __m128i expK
 		startNum = threadNum * stepSize + 1;
 		stopNum = startNum + stepSize - 1;
 	}
-	
+}
+*/
 
+void thrAes5(const uint64_t startNum, const uint64_t stopNum, __m128i expKey[], uint8_t res[])
+{
 	__m128i mes128;
 	__m128i out;
 
-	for (uint32_t i = startNum; i <= stopNum; ++i)
+	for (uint64_t i = startNum; i <= stopNum; ++i)
 	{
 		
 		// build plaintext
@@ -168,10 +300,37 @@ void thrAes5(const uint32_t &threadNum, const uint32_t &numThreads, __m128i expK
 
 		
 		mtx.lock();
-		res[i] = res[i] + 1;
+		res[diag] = res[diag] + 1;
 		mtx.unlock();
 	}
 
 }
+
+void printUsage(std::string name)
+{
+	std::cerr << "Usage: " << name << std::endl
+		<< "\t\t<MODE>        FULL, PER_THREAD, TOTAL" << std::endl
+		<< "\t\t<WORKLOAD>    Specifies the number of computations, depending on the mode. Ignored if MODE = FULL" << std::endl
+		<< "\t\t<NUM_THREADS> Optional. Specifies the number of threads. Default = 1" << std::endl;
+
+	std::cerr << std::endl << "\t\tExample: " << name << " FULL 0" << std::endl
+		<< "\t\tStarts the differential distinguisher with 1 thread and does the full computation." << std::endl;
+
+	std::cerr << std::endl << "\t\tExample: " << name << " TOTAL 50 3" << std::endl
+		<< "\t\tStarts the differential distinguisher with 3 threads and does 50 computations in total." << std::endl;
+}
+
+StepSizeSetting parseSetting(std::string setting)
+{
+	for (auto & c : setting) c = toupper(c);
+	if (strcmp(setting.c_str(), "PER_THREAD") == 0)
+		return PER_THREAD;
+	else if (strcmp(setting.c_str(), "TOTAL") == 0)
+		return TOTAL;
+	else
+		return FULL;
+}
+
+
 
 #endif
